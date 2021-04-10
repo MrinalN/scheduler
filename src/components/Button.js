f23da5080fd60afce0149ab8894cc720f9debe53
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-10 08:42:00 -0700

    Check npm test removing button return

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,6 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   //removing return below made npm test fail 
+   return <></>; 
 }
