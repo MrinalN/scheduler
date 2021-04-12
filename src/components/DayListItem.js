61cf052b060589c35a591cf0860ed0193324ed8f
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-12 16:19:35 -0700

    Add scss and component conditional styling

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,16 @@
 import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
   const { name, spots, selected, setDay } = props
+
+  let dayListItemClass = classNames('day-list__item', {
+    'day-list__item--selected': selected, 'day-list__item--full': spots === 0
+  })
   return (
-    <li onClick={() => props.setDay(name)}>
+    <li className={dayListItemClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{spots}</h3>
     </li>
