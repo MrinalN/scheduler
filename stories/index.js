986646d1938a50d203a07be2537b204f28de8363
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-13 15:44:39 -0700

    Add Error component, pass props, functional

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Empty from "components/Appointment/Empty.js"
 import Show from "components/Appointment/Show.js"
 import Confirm from "components/Appointment/Confirm.js"
 import Status from "components/Appointment/Status.js"
+import Error from "components/Appointment/Error.js"
 
 
 //input data
@@ -180,4 +181,11 @@ storiesOf("Button", module)
 
   .add("Status", () => (
     <Status message={"Deleting"} />
+  ))
+
+  .add("Error", () => (
+    <Error 
+    message={"Could not delete appointment."}
+    onClose={action("onClose")}
+    />
   ))
