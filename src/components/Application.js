0cf437784efae51087b4e1a925983f2a9e6d2b8a
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-12 19:09:32 -0700

    Add useState hook. Function not active yet

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
@@ -22,6 +23,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,9 +36,9 @@ export default function Application(props) {
           <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
             <DayList
-              days={days}
-              day={"Monday"}
-              setDay={day => console.log(day)}
+             days={days} 
+             day={day} 
+             setDay={setDay}
             />
           </nav>
           <img
