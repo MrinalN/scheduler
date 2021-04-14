c74a09f927c938066f1ab7b6e3ebe56cac1e35d0
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-14 16:31:56 -0700

    Consolidate state, setState -Establish setDays and setDay functionality

--- src/components/Application.js
+++ src/components/Application.js
@@ -72,8 +72,15 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     const daysURL = '/api/days';
@@ -98,8 +105,8 @@ export default function Application(props) {
           <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
             <DayList
-             days={days} 
-             day={day} 
+             days={state.days} 
+             day={state.day} 
              setDay={setDay}
             />
           </nav>
