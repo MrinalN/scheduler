952fbdd7f8a1e8429782063cb46d9624aa38a5d8
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-14 11:17:51 -0700

    Add appt component to display w conditional

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
+import Appointment from "./Appointment/index.js";
 import DayList from "./DayList";
 
 const days = [
@@ -22,8 +23,58 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "12pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Bob Dylan",
+      interviewer: {
+        id: 1,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "11am",
+    interview: {
+      student: "Archie Bunker",
+      interviewer: {
+        id: 1,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const parsedAppointments = appointments.map(appointment => 
+    <Appointment key={appointment.id} {...appointment} />
+    )
   return (
     <main className="layout">
       <section className="sidebar">
@@ -49,7 +100,10 @@ export default function Application(props) {
         </>}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+          <li>
+            {parsedAppointments}
+            <Appointment key="last" time="5pm" />
+          </li>
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,91 +7,21 @@ import Empty from 'components/Appointment/Empty.js';
 import './styles.scss';
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "12pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "11am",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
-
-// import Header from './Header';
-
 export default function Appointment (props) {
   const { time, interview } = props;
-
-  // const variable = interview ? <Show /> : <Empty />
+  const displayAppt = interview ? 
+  <Show 
+    student={interview.student}
+    interviewer={interview.interviewer}
+  /> : <Empty />
   return(
     <article className="appointment">
       <Header time={time}/>
 
-    {interview && <Show 
-    student={interview.student}
-    interviewer={interview.interviewer}
-    />}
+    {displayAppt}
 
-    {/* {!interview && <Empty 
-    
-    />} */}
-      {/* {variable} */}
     </article>
 
     
   )
 }
-
-// storiesOf("Appointment", module)
-//   .addParameters({
-//     backgrounds: [{ name: "white", value: "#fff", default: true }]
-//   })
-//   .add("Appointment", () => (
-//   <Appointment />
-//   ))
-
-//   .add("Appointment with Time", () => (
-//     <Appointment
-//       time="12pm"
-//     />
-//   ))
-//   .add("Header", () => (
-//     <Header time="12pm" />
-//   ))
