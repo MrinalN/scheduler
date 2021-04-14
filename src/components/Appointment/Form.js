3512c713c0163e96639708732257eeca459c5596
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-13 19:11:56 -0700

    Create Form compent pass props

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+import InterviewerList from "../InterviewerList";
+import Button from "../Button"
+
+
+// name={"Lydia Miller-Jones"}
+// interviewers={interviewers}
+// interviewer={1}
+// onSave={action("onSave")}
+// onCancel={action("onCancel")}
 
 export default function Form (props) {
+  // const {name, interviewers, interviewer, onSave, onCancel} = props;
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  
+
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
-    <form autoComplete="off">
+    <form autoComplete="off" >
       <input
         className="appointment__create-input text--semi-bold"
         name="name"
         type="text"
-        placeholder="Enter Student Name"
+        placeholder={"Enter Student Name"}
+        value={name}
+        onChange={(event) => setName(event.target.value)}
         /*
           This must be a controlled component
         */
--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,9 @@ import Show from "components/Appointment/Show.js"
 import Confirm from "components/Appointment/Confirm.js"
 import Status from "components/Appointment/Status.js"
 import Error from "components/Appointment/Error.js"
+// import Edit from "components/Appointment/Form.js"
+// import Create from "components/Appointment/Form.js"
+import Form from "components/Appointment/Form.js"
 
 
 //input data
@@ -190,9 +193,20 @@ storiesOf("Button", module)
     />
   ))
 
+  .add("Edit", () => (
+    <Form 
+    name={"Lydia Miller-Jones"}
+    interviewers={interviewers}
+    interviewer={1}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+    />
+  ))
 
-//   name:String
-// interviewers:Array
-// interviewer:Number
-// onSave:Function
-// onCancel:Function
+  .add("Create", () => (
+    <Form 
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+    />
+  ))
