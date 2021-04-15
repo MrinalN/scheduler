960fba3ea0fed08b3a75b498d52d17a056595245
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-15 16:19:28 -0700

    useVisualMode imported. 3 conditional components updated

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,22 +3,64 @@ import React from 'react';
 import Header from 'components/Appointment/Header.js';
 import Show from 'components/Appointment/Show.js';
 import Empty from 'components/Appointment/Empty.js';
+import Form from 'components/Appointment/Form.js';
+import useVisualMode from 'hooks/useVisualMode';
 
 import './styles.scss';
 
 
 export default function Appointment (props) {
-  const { time, interview } = props;
-  const displayAppt = interview ? 
-  <Show 
-    student={interview.student}
-    interviewer={interview.interviewer}
-  /> : <Empty />
+  // const { time, interview } = props;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  // const STATUS = "STATUS";
+  // const CONFIRM = "CONFIRM";
+  // const ERROR = "ERROR";
+  // const EDIT = "EDIT";
+  const CREATE = "CREATE";
+  // const SAVE = "SAVE";
+
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  // const displayAppt = interview ? 
+  // <Show 
+  //   student={interview.student}
+  //   interviewer={interview.interviewer}
+  // /> : <Empty />
+
+
+  // if (mode === EMPTY) {
+  //   transition(CREATE);
+  // }
+
+
+
   return(
     <article className="appointment">
-      <Header time={time}/>
+      <Header time={props.time}/>
+      {mode === EMPTY && 
+      <Empty onAdd={() => transition(CREATE)} 
+      />
+      }
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (    
+        <Form 
+          interviewers={props.interviewers}
+          // onSave={() => ("onSave")}
+          onCancel={() => (back)}
+        />
+      )}
 
-    {displayAppt}
+
+    {/* {displayAppt} */}
 
     </article>
 
