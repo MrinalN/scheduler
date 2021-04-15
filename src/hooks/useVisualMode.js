e7402f40d91ed5366a0cc3cb93589bd3c63051d2
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-15 15:13:14 -0700

    Add conditional to transisition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,9 +4,11 @@ export default function useVisualMode (initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]); 
 
-  function transition (nextMode) {
+  function transition (nextMode, replace = false) { 
     setMode(nextMode)
-    setHistory(prev => ([...prev, nextMode]))
+    // setHistory(prev => ([...prev, nextMode]))
+
+    replace === true ? setHistory (prev => ([...prev])) : setHistory(prev => ([...prev, nextMode])) 
   }
   
   function back() {
