ebc59d8029ae666f57fc051de2cfb6ec9aa4f694
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-04-23 13:35:34 -0700

    Fix bug in function and selectors test

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -6,12 +6,14 @@ const state = {
     {
       id: 1,
       name: "Monday",
-      appointments: [1, 2, 3]
+      appointments: [1, 2, 3],
+      interviewers:[1, 2]
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5]
+      appointments: [4, 5],
+      interviewers:[1, 2]
     }
   ],
 
@@ -97,23 +99,23 @@ test("getInterview returns null if no interview is booked", () => {
 
 
 test("getInterviewersForDay returns an array", () => {
-  const result = getInterviewersForDay(state, "Wednesday");
+  const result = getInterviewersForDay(state, "Monday");
   expect(Array.isArray(result)).toBe(true);
 });
 
 test("getInterviewersForDay returns an array with a length matching the number of appointments for that day", () => {
-  const result = getInterviewersForDay(state, "Friday");
-  expect(result.length).toEqual(0);
+  const result = getInterviewersForDay(state, "Monday");
+  expect(result.length).toEqual(2);
 });
 
 test("getInterviewersForDay returns an array containing the correct appointment objects", () => {
-  const [first, second] = getInterviewersForDay(state, "Thursday");
-  expect(first).toEqual(state.interviewers["4"]);
-  expect(second).toEqual(state.interviewers["5"]);
+  const [first, second] = getInterviewersForDay(state, "Tuesday");
+  expect(first).toEqual(state.interviewers["1"]);
+  expect(second).toEqual(state.interviewers["2"]);
 });
 
 test("getInterviewersForDay returns an empty array when the days data is empty", () => {
-  const result = getInterviewersForDay({ days: [] }, "Wednesday");
+  const result = getInterviewersForDay({ days: [] }, "Monday");
   expect(result.length).toEqual(0);
 });
 
